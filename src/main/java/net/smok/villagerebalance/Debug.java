60ac897e284edcac2b794aeba11b5936698b73e2
Smok419 <danilmaksimovih@gmail.com>
2025-06-07 08:35:43 +0300

    Fabric version create

--- src/main/java/net/smok/villagerebalance/Debug.java
+++ src/main/java/net/smok/villagerebalance/Debug.java
@@ -1,35 +1,40 @@
 package net.smok.villagerebalance;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Debug {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Values.MOD_ID);
+
     public static void log(String message) {
-        Values.LOGGER.info(message);
+        LOGGER.info(message);
     }
 
     public static void log(String message, Object... objects) {
-        Values.LOGGER.info(message, objects);
+        LOGGER.info(message, objects);
     }
 
     public static void warn(String message) {
-        Values.LOGGER.warn(message);
+        LOGGER.warn(message);
     }
 
     public static void warn(String message, Object... objects) {
-        Values.LOGGER.warn(message, objects);
+        LOGGER.warn(message, objects);
     }
 
     public static void warn(String message, Throwable throwable) {
-        Values.LOGGER.warn(message, throwable);
+        LOGGER.warn(message, throwable);
     }
 
     public static void err(String message) {
-        Values.LOGGER.error(message);
+        LOGGER.error(message);
     }
 
     public static void err(String message, Object... objects) {
-        Values.LOGGER.error(message, objects);
+        LOGGER.error(message, objects);
     }
 
     public static void err(String message, Throwable throwable) {
-        Values.LOGGER.error(message, throwable);
+        LOGGER.error(message, throwable);
     }
 }
--- src/main/java/net/smok/villagerebalance/Values.java
+++ src/main/java/net/smok/villagerebalance/Values.java
@@ -1,12 +1,8 @@
 package net.smok.villagerebalance;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public final class Values {
 
     public static final String MOD_ID = "village_rebalance";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
         Debug.log("Values are initialized.");
